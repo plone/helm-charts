b778af03e48fc02a3067633bbad3a1b473626894
Fabiano Weimar <xiru@xiru.org>
2024-04-11 12:35:16 -0300

    Add tests.

--- test.sh
+++ test.sh
@@ -11,14 +11,16 @@ for cmd in kubectl helm; {
 }
 
 function finish {
-  kubectl delete namespace plone-helm-test
+  kubectl delete namespace plone-helm
 }
 trap finish EXIT
 
-kubectl create namespace plone-helm-test
+kubectl create namespace plone-helm
 
 for helm_name in plone6-volto-zeo plone6-classic-pg plone6-volto-pg plone6-volto-pg-nginx-varnish; {
-    echo Testing $helm_name;
-    helm install -n plone-helm-test plone-helm-$helm_name ./$helm_name && sleep 30 && \
-    helm uninstall -n plone-helm-test plone-helm-$helm_name
+    echo Testing $helm_name
+    helm dependency update ./$helm_name
+    helm dependency list ./$helm_name
+    helm install -n plone-helm dummy ./$helm_name && sleep 10 && \
+    helm uninstall -n plone-helm dummy
 }
